e9d5f70f324e5d62e4f187018ab9c36eb69118d2
Matt Gillooly <matt@fusecal.com>
2008-10-14 00:01:13 -0400

    add config info to use 'oblique' app name

--- obtweet.rb
+++ obtweet.rb
@@ -12,6 +12,13 @@ class Array
   end
 end
 
+Twitter::Client.configure do |conf|
+  conf.application_name = 'oblique'
+  conf.application_url = 'http://twitter.com/oblique'
+  conf.application_version = '0.1'
+  conf.source = 'oblique'
+end
+
 config_filename = File.join(File.dirname(__FILE__), 'twitter.yml')
 twitter = Twitter::Client.from_config(config_filename)
 

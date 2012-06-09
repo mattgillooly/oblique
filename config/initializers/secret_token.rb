5003feeb7b91a5a5bef7cc0acc2320a96310a7bf
Matt Gillooly <matt@mattgillooly.com>
2012-06-09 17:02:09 -0400

    now I'm a Rails app!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oblique::Application.config.secret_token = '8e33c511489c5eb2719484bcaf06b16a1fe3296e6751be474d114bb9e4f41e7c4d262c6596e763c2a72df80856db2e73f0248f842dab5ec314961a020f6316b1'

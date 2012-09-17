3c1630017de9747e8562d963dcb834cd039301e2
Matt Gillooly <matt@mattgillooly.com>
2012-09-16 23:13:52 -0400

    don't post tweets via rake in cucumber features

--- features/support/twitter.rb
+++ features/support/twitter.rb
@@ -1,10 +1,11 @@
-require "rake"
-
 module TwitterRakeHelpers
   def invoke_twitter_rake(task_name)
-    load File.join(Rails.root, 'lib', 'tasks', 'twitter.rake')
-    Rake::Task.define_task(:environment)
-    Rake::Task[task_name].invoke
+    case task_name
+    when 'post_random_tweets'
+      RandomTweeter.new.post_tweets
+    else
+      raise "don't know how to run task: #{task_name}"
+    end
   end
 end
 
